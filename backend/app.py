e8fee513564b24dee4180b2a0443f58809feb39d
Yassir Abbassi <y26361442@gmail.com>
2025-06-11 12:44:18 +0100

    Version 0.3.3

--- backend/app.py
+++ backend/app.py
@@ -2,17 +2,22 @@
 
 # Import libraries
 from flask import Flask, request, send_file, jsonify        # Flask WSGI, REST API, JSON
-from flask_cors import CORS                                 # Back-end API Routing Resource Restriction (Security Measure)
-import requests
-import os
-from io import BytesIO
-from dotenv import load_dotenv
+from flask_cors import CORS                                 # Back-end API routing resources restriction (Security Measure)
 
+import base64                                               # Required by SDXL 1.0
+import os
+import requests
+
+from io import BytesIO
+from dotenv import load_dotenv                              # Load .env variables
+
+# Initiate instances
 load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
+# Load env variables
 LEGACY_API_HOST = os.getenv("STABILITY_LEGACY_API_HOST")
 API_KEY = os.getenv("STABILITY_API_KEY")
 ENGINE_ID = os.getenv("STABILITY_API_ENGINE_ID")
@@ -25,13 +30,8 @@ HEADERS_IMAGE = {
     "Accept": "image/*"
 }
 
-def fetch_image_from_stability(url, headers, files_or_json):
-    response = requests.post(url, headers=headers, **files_or_json)
-    if response.status_code != 200:
-        return None, response.text
-    return BytesIO(response.content), None
-
-
+# Routes Definition
+## ROUTE A - Generate image
 @app.route("/generate", methods=["POST"])
 def generate_image():
     data = request.get_json()
@@ -65,6 +65,7 @@ def generate_image():
     return send_file(BytesIO(base64.b64decode(image_data)), mimetype="image/png")
 
 
+## ROUTE B - Inpaint mask in image
 @app.route("/inpaint", methods=["POST"])
 def inpaint_image():
     prompt = request.form.get("prompt")
@@ -77,10 +78,10 @@ def inpaint_image():
     files = {
         "image": (image.filename, image.stream, image.mimetype)
     }
-    
+
     if mask:
         files["mask"] = (mask.filename, mask.stream, mask.mimetype)
-    
+
     response = requests.post(
         "https://api.stability.ai/v2beta/stable-image/edit/inpaint",
         headers=HEADERS_IMAGE,
@@ -91,12 +92,22 @@ def inpaint_image():
         },
     )
 
+    # Debugging
+    #print("Status code:", response.status_code)
+    #print("Returned bytes:", len(response.content))
+    #print("Content-Type:", response.headers.get("Content-Type"))
+
     if response.status_code != 200:
         return jsonify(response.json()), response.status_code
+
+    # Save the file locally
+    #with open("debug_output.png", "wb") as f:
+    #    f.write(response.content)
 
     return send_file(BytesIO(response.content), mimetype="image/png")
 
 
+## ROUTE C - Erase mask from image
 @app.route("/erase", methods=["POST"])
 def erase_image():
     image = request.files.get("image")
